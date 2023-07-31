bce1568e6890719cc671d785ca7da138f5608575
ESTEBAN LÓPEZ RIZZOLO <47069838@ort>
2023-07-31 12:00:24 -0300

    CSS ACTUALIZADO

--- frontend/src/App/Lugar/Lugar.js
+++ frontend/src/App/Lugar/Lugar.js
@@ -10,62 +10,86 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
-
-
-
+import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function Lugar() {
     const [lugares, setLugares] = useState([]);
+    const [ubicacionSeleccionada, setUbicacionSeleccionada] = useState('');
     const Navigate = useNavigate('');
+
     useEffect(() => {
         console.log("LMAO");
         axios.get("http://localhost:5001/lugar")
             .then(res => {
                 console.log("AXIOSRES", res)
-                setLugares(
-                    res.data
-                );
-            })
-    }, [])
+                setLugares(res.data);
+            });
+    }, []);
 
     const navigateToCanchas = (Id) => {
-        Navigate('/Canchas/', {state: {id: Id}});
+        Navigate('/Canchas/', { state: { id: Id } });
     }
 
-    
-    if(lugares.length === 0) return (<div></div>);
-    document.body.classList = ["Lugar"];
-    return (
-       
- 
-        <Container>
-        <Row>             
-                {
-                   
-              
-                    lugares.map((element) =>
-                    (
-                    <Col sm={6}>
-                    <Card >
-                        <Card.Header><img className="card-img-top" src={element.Foto} alt="Card image cap"></img></Card.Header>
-                        <Card.Body>
-                            <Card.Title><h1>{element.Nombre}</h1></Card.Title>
-                            <Card.Text>
-                                {element.Ubicacion}<br/><br/>
-                                Zona: {element.Zona}
-                                
-                            </Card.Text>
-                            <Button onClick={() => navigateToCanchas(element.Id)} variant="primary">Ver más</Button>
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                    )
-                    )
-                }
-                </Row> 
-                </Container>
+    const handleUbicacionSelect = (ubicacion) => {
+        setUbicacionSeleccionada(ubicacion);
+    }
 
+
+    const lugaresFiltrados = lugares.filter((lugar) =>
+        ubicacionSeleccionada === '' || lugar.Ubicacion === ubicacionSeleccionada
+    );
+
+    if (lugares.length === 0) return (<div></div>);
+    document.body.classList = ["Lugar"];
+
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">Nombre de tu App</Navbar.Brand>
+
+
+                <Nav className="mr-auto">
+                        <Nav.Link href="#home">Inicio </Nav.Link>
+                        <Nav.Link href="#link">Otra página</Nav.Link>
+                    </Nav>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    
+                </Navbar.Collapse>
+            </Navbar>
+
+            <Container>
+
+                <Dropdown>
+                    <Dropdown.Toggle variant="primary" id="dropdown-ubicacion">
+                        Filtrar por Zona: {ubicacionSeleccionada || 'Todas'}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => handleUbicacionSelect('')}>Todas</Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleUbicacionSelect('Caballito')}>Caballito</Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleUbicacionSelect('Flores')}>Flores</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Row>
+                    {lugaresFiltrados.map((element) => (
+                        <Col sm={6}>
+                            <Card>
+                                <Card.Header><img className="card-img-top" src={element.Foto} alt="Card image cap"></img></Card.Header>
+                                <Card.Body>
+                                    <Card.Title><h1>{element.Nombre}</h1></Card.Title>
+                                    <Card.Text>
+                                        {element.Ubicacion}<br /><br />
+                                        Zona: {element.Zona}
+                                    </Card.Text>
+                                    <Button onClick={() => navigateToCanchas(element.Id)} variant="primary">Ver más</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </div>
     );
 }
 
