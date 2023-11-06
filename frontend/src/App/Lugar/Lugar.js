023cab05e20294e0d328d25c5e96a5028804762f
ESTEBAN LÓPEZ RIZZOLO <47069838@ort>
2023-11-06 09:14:45 -0300

    sambalanga

--- frontend/src/App/Lugar/Lugar.js
+++ frontend/src/App/Lugar/Lugar.js
@@ -5,14 +5,13 @@ import Button from "react-bootstrap/Button";
 import "./Lugar.css";
 import { Navigate } from "react-router-dom";
 import axios from 'axios';
-import { useFetcher, useNavigate} from 'react-router-dom';
+import { useFetcher, useNavigate } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Dropdown from 'react-bootstrap/Dropdown';
-import NavLink from "react-bootstrap/esm/NavLink";
 import LogoCancheros from '../../Logo.png'
 import usuarioContext from "../../Context/context";
 
@@ -24,37 +23,35 @@ function Lugar() {
 
     useEffect(() => {
         console.log("LMAO");
-        console.log("ROL:" , context.usuario.fkRol)
-        console.log("ID:",context.usuario.Id)
+        console.log("ROL:", context.usuario.fkRol)
+        console.log("ID:", context.usuario.Id)
         if (context.usuario.fkRol === 2) {
             axios.get("http://localhost:5001/lugar/" + context.usuario.Id)
-            .then(res => {
-                console.log("AXIOSRES", res)
-                setLugares(res.data);
-            });
-        }
-        else if (context.usuario.fkRol === 3){
+                .then(res => {
+                    console.log("AXIOSRES", res)
+                    setLugares(res.data);
+                });
+        } else if (context.usuario.fkRol === 3) {
             axios.get("http://localhost:5001/lugar")
-            .then(res => {
-                console.log("AXIOSRES", res)
-                setLugares(res.data);
-            });
+                .then(res => {
+                    console.log("AXIOSRES", res)
+                    setLugares(res.data);
+                });
         }
     }, []);
 
-    
     const navigateToCanchas = (IdL) => {
         console.log("lugarID", IdL)
         Navigate('/Canchas/' + IdL);
     }
-    const navigateToHome = () => { 
+
+    const navigateToHome = () => {
         Navigate('/');
     }
 
     const handleZonaSelect = (Zona) => {
         setZonaSeleccionada(Zona);
     }
-
 
     const lugaresFiltrados = lugares.filter((lugar) =>
         zonaSeleccionada === '' || lugar.Zona === zonaSeleccionada
@@ -64,60 +61,12 @@ function Lugar() {
     document.body.classList = ["Lugar"];
     return context.usuario.fkRol !== 2 ? (
         <div className="Fondo">
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand onClick={navigateToHome}><img className="LogoLugar" src={LogoCancheros}></img></Navbar.Brand>
-            <Nav className="mr-auto">
-                    <Nav.Link onClick={() => Navigate("/FormDueño")}>Unirse como dueño </Nav.Link>
-                </Nav>
-            <Navbar.Collapse id="basic-navbar-nav">
-
-            </Navbar.Collapse>
-        </Navbar>
-
-        <Container>
-
-            <Dropdown>
-                <Dropdown.Toggle variant="primary" id="dropdown-ubicacion">
-                    Filtrar por Zona: {zonaSeleccionada || 'Todas'}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => handleZonaSelect('')}>Todas</Dropdown.Item>
-                    <Dropdown.Item onClick={() => handleZonaSelect('Caballito')}>Caballito</Dropdown.Item>
-                    <Dropdown.Item onClick={() => handleZonaSelect('Flores')}>Flores</Dropdown.Item>
-                    
-                </Dropdown.Menu>
-            </Dropdown>
-
-            <Row>
-                {lugaresFiltrados.map((element) => (
-                    <Col sm={6}>
-                        <Card>
-                            <Card.Header><img className="card-img-top" src={element.Foto} alt="Card image cap"></img></Card.Header>
-                            <Card.Body>
-                                <Card.Title><h1>{element.Nombre}</h1></Card.Title>
-                                <Card.Text>
-                                    {element.Ubicacion}<br /><br />
-                                    Zona: {element.Zona}
-                                </Card.Text>
-                                <Button onClick={() => navigateToCanchas(element.Id)} variant="primary">Ver más</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-        </Container>
-    </div>
-    ) : 
-   (
-        <div className="Fondo">
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand onClick={navigateToHome}><img className="LogoLugar" src={LogoCancheros}></img></Navbar.Brand>
+                <Navbar.Brand onClick={navigateToHome}><img className="LogoLugar" src={LogoCancheros} alt="Logo Cancheros" /></Navbar.Brand>
                 <Nav className="mr-auto">
-                        <Nav.Link href="/CrearCancha">Crear cancha</Nav.Link>
-                    </Nav>
-                <Navbar.Collapse id="basic-navbar-nav">
-
-                </Navbar.Collapse>
+                    <Nav.Link onClick={() => Navigate("/FormDueño")}>Unirse como dueño</Nav.Link>
+                </Nav>
+                <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
             </Navbar>
 
             <Container>
@@ -130,19 +79,23 @@ function Lugar() {
                         <Dropdown.Item onClick={() => handleZonaSelect('')}>Todas</Dropdown.Item>
                         <Dropdown.Item onClick={() => handleZonaSelect('Caballito')}>Caballito</Dropdown.Item>
                         <Dropdown.Item onClick={() => handleZonaSelect('Flores')}>Flores</Dropdown.Item>
-                        
                     </Dropdown.Menu>
                 </Dropdown>
 
                 <Row>
                     {lugaresFiltrados.map((element) => (
                         <Col sm={6}>
-                            <Card>
-                                <Card.Header><img className="card-img-top" src={element.Foto} alt="Card image cap"></img></Card.Header>
+                            <Card className="custom-card">
+                                <Card.Header>
+                                    <img className="card-img-top" src={element.Foto} alt="Card image cap" />
+                                </Card.Header>
                                 <Card.Body>
-                                    <Card.Title><h1>{element.Nombre}</h1></Card.Title>
+                                    <Card.Title>
+                                        <h1>{element.Nombre}</h1>
+                                    </Card.Title>
                                     <Card.Text>
-                                        {element.Ubicacion}<br /><br />
+                                        {element.Ubicacion}
+                                        <br /><br />
                                         Zona: {element.Zona}
                                     </Card.Text>
                                     <Button onClick={() => navigateToCanchas(element.Id)} variant="primary">Ver más</Button>
@@ -153,6 +106,55 @@ function Lugar() {
                 </Row>
             </Container>
         </div>
-    );
-}
+    ) :
+        (
+            <div className="Fondo">
+                <Navbar className="barraLogo"  expand="lg">
+                    <Navbar.Brand  onClick={navigateToHome}> <a className="textoLogo">CANCHEROS</a><img className="LogoLugar" src={LogoCancheros} alt="Logo Cancheros" /></Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/CrearCancha">Crear cancha</Nav.Link>
+                    </Nav>
+                    <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+                </Navbar>
+
+                <Container>
+
+                    <Dropdown>
+                        <Dropdown.Toggle variant="primary" id="dropdown-ubicacion">
+                            Filtrar por Zona: {zonaSeleccionada || 'Todas'}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => handleZonaSelect('')}>Todas</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleZonaSelect('Caballito')}>Caballito</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleZonaSelect('Flores')}>Flores</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Row>
+                        {lugaresFiltrados.map((element) => (
+                            <Col sm={6}>
+                                <Card className="custom-card">
+                                    <Card.Header>
+                                        <img className="card-img-top" src={element.Foto} alt="Card image cap" />
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            <h1>{element.Nombre}</h1>
+                                        </Card.Title>
+                                        <Card.Text>
+                                            {element.Ubicacion}
+                                            <br /><br />
+                                            Zona: {element.Zona}
+                                        </Card.Text>
+                                        <Button onClick={() => navigateToCanchas(element.Id)} variant="primary">Ver más</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
+            </div>
+        );
+                        }
+
 export default Lugar;
